1a40221281b94baa27acbb44613fdd9115ac5eec
757356 <757356249@qq.com>
2021-12-08 13:15:29 +0800

    7

--- app/src/main/java/cn/itcast/experiment/WebViewFragment.java
+++ app/src/main/java/cn/itcast/experiment/WebViewFragment.java
@@ -34,7 +34,7 @@ public class WebViewFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootView = inflater.inflate(R.layout.fragment_web_view, container, false);
         WebView webView=rootView.findViewById(R.id.webview_content);
-        webView.loadUrl("www.baidu.com");
+        webView.loadUrl("http://www.sina.com");
         WebSettings webSettings=webView.getSettings();
         webSettings.setJavaScriptEnabled(true);
         return rootView;
